c77bf9dcf2077092c7e4a547efeda5d620dc82b2
Coding-with-zaid <161318100+Coding-with-zaid@users.noreply.github.com>
2024-02-26 04:07:12 -0800

    Add files via upload

---
+++ Typescript_Exercises 01/app.ts
@@ -0,0 +1,17 @@
+let message = "hellow world !";
+console.log(message);  
+
+// var member = 'zaid';
+// console.log(member);
+
+// var member = 'ahmed';
+// console.log(member);
+
+// let Name =  'safwan';
+// Name = 'Shahjee';
+// console.log(Name);
+
+
+
+
+
